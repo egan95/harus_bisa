4c130afacc2a4ca128bf550d970890f05137a9f4
eg4n <egantkj@gmail.com>
2014-12-16 21:23:34 +0700

    upload

---
+++ src/harus/Harus.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package harus;
+
+/**
+ *
+ * @author eg4n
+ */
+public class Harus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //ini untuk cabang satu
+        
+        //ini untuk cabang dua
+    }
+    
+}
